90a96b8b9ca883cccadccf6544109a0a00294208
macedo-erick <macedo.eriick@gmail.com>
2026-01-27 19:11:14 -0300

    feat: add API Gateway configuration with Swagger documentation and service routing

--- api-gateway/src/main/java/br/com/operix/apigateway/config/SwaggerConfig.java
+++ api-gateway/src/main/java/br/com/operix/apigateway/config/SwaggerConfig.java
@@ -3,8 +3,11 @@ package br.com.operix.apigateway.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -17,6 +20,20 @@ public class SwaggerConfig {
                         .description("API Gateway - Central point for Operix Microservices")
                         .version("1.0.0")
                         .contact(new Contact()
-                                .name("Operix Team")));
+                                .name("Operix Team")))
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:8080")
+                                .description("API Gateway"),
+                        new Server()
+                                .url("http://localhost:8081")
+                                .description("Product Service"),
+                        new Server()
+                                .url("http://localhost:8082")
+                                .description("People Service"),
+                        new Server()
+                                .url("http://localhost:8083")
+                                .description("Inventory Service")
+                ));
     }
 }
