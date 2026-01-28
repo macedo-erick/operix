4e44cd41a414ffbb4e3171596f21f45c52bec469
macedo-erick <macedo.eriick@gmail.com>
2026-01-27 22:11:56 -0300

    feat: refactor API Gateway configuration and restore service routes

--- api-gateway/src/main/java/br/com/operix/apigateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/br/com/operix/apigateway/config/GatewayConfig.java
@@ -1,28 +1,37 @@
 package br.com.operix.apigateway.config;
 
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.LoadBalancerFilterFunctions.lb;
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.web.servlet.function.RequestPredicates.path;
+
 @Configuration
 public class GatewayConfig {
 
-//    @Bean
-//    public RouterFunction<ServerResponse> productServiceRoute() {
-//        return route("product-service")
-//                .route(path("/api/products/**"), HandlerFunctions.http())
-//                .filter(lb("product-service"))
-//                .build();
-//    }
-//    @Bean
-//    public RouterFunction<ServerResponse> peopleServiceRoute() {
-//        return route("people-service")
-//                .route(path("/api/people/**"), HandlerFunctions.http())
-//                .filter(lb("people-service"))
-//                .build();
-//    }
-//    @Bean
-//    public RouterFunction<ServerResponse> inventoryServiceRoute() {
-//        return route("inventory-service")
-//                .route(path("/api/inventory/**"), HandlerFunctions.http())
-//                .filter(lb("inventory-service"))
-//                .build();
-//    }
+    @Bean
+    public RouterFunction<ServerResponse> productServiceRoute() {
+        return route("product-service")
+                .route(path("/api/products/**"), HandlerFunctions.http())
+                .filter(lb("product-service"))
+                .build();
+    }
+    @Bean
+    public RouterFunction<ServerResponse> peopleServiceRoute() {
+        return route("people-service")
+                .route(path("/api/people/**"), HandlerFunctions.http())
+                .filter(lb("people-service"))
+                .build();
+    }
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceRoute() {
+        return route("inventory-service")
+                .route(path("/api/inventory/**"), HandlerFunctions.http())
+                .filter(lb("inventory-service"))
+                .build();
+    }
 }
--- product-service/src/main/java/br/com/operix/productservice/service/ProductService.java
+++ product-service/src/main/java/br/com/operix/productservice/service/ProductService.java
@@ -7,8 +7,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import lombok.NoArgsConstructor;
-
 import java.util.UUID;
 
 @Service
